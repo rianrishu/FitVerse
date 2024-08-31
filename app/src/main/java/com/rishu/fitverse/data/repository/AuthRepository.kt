bf75f48acf5c11bbe2ee6ff6508d3a6383a21e8f
rianrishu <rishuanand2552@gmail.com>
2024-08-31 23:51:32 +0530

    crashlytics config

--- app/src/main/java/com/rishu/fitverse/data/repository/AuthRepository.kt
+++ app/src/main/java/com/rishu/fitverse/data/repository/AuthRepository.kt
@@ -1,4 +1,28 @@
 package com.rishu.fitverse.data.repository
 
-class AuthRepository {
+import com.rishu.fitverse.data.api.local.datasource.PreferencesDataSource
+import com.rishu.fitverse.data.api.remote.datasource.FirebaseAuthDataSource
+import com.rishu.fitverse.data.api.remote.datasource.HarperDbAuthDataSource
+import com.rishu.fitverse.data.model.mapper.UserMapper
+import com.rishu.fitverse.utils.NetworkUtils
+import javax.inject.Inject
+import javax.inject.Named
+
+class AuthRepository @Inject constructor(
+    private val authDataSource: FirebaseAuthDataSource,
+    private val harperDbAuthDataSource: HarperDbAuthDataSource,
+    @Named("sharedPref") private val preferencesDataSource: PreferencesDataSource,
+    private val userMapper: UserMapper,
+    private val networkUtils: NetworkUtils
+) {
+    suspend fun logoutUser() {
+        authDataSource.logoutUser()
+        preferencesDataSource.removeUserData()
+    }
+
+    private suspend fun removeUser() {
+        authDataSource.removeUser()
+        preferencesDataSource.removeUserData()
+    }
+
 }
