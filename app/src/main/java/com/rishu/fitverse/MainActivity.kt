7dcda6682b6c420e60aa2206d6e5ff6e776e0b4c
rianrishu <rishuanand2552@gmail.com>
2024-08-31 15:57:45 +0530

    made status bar transparent

--- app/src/main/java/com/rishu/fitverse/MainActivity.kt
+++ app/src/main/java/com/rishu/fitverse/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rishu.fitverse.theme.FitVerseTheme
+import com.rishu.fitverse.utils.makeStatusBarTransparent
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        makeStatusBarTransparent()
         setContent {
             FitVerseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
